a80f686b18dcc5da931ba53e87b939e4ee10962f
Jorge Ruiz López <146338081+jorge-ruizl-um-es@users.noreply.github.com>
2025-10-10 19:36:27 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,9 +4,6 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT', 5000))
-
-	# Usar debug solo en desarrollo, no en producción
+    port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
-	
-	app.run(host='0.0.0.0', port=port, debug=debug)
+    app.run(host='0.0.0.0', port=port, debug=debug)
