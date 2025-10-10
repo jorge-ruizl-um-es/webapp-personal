9e8ccee0e82d96fcef72c2eb4cf95ce60d1afc80
Jorge Ruiz López <146338081+jorge-ruizl-um-es@users.noreply.github.com>
2025-10-10 19:55:34 +0200

    Update run.py changing port

--- run.py
+++ run.py
@@ -4,6 +4,6 @@ import os
 app = create_app()
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     debug = os.environ.get('FLASK_DEBUG', 'False').lower() == 'true'
     app.run(host='0.0.0.0', port=port, debug=debug)
